6a3fa34ad23f183e1f2a380cbea43b23392ef72c
noluzar <noluthandosamantha.ns@gmail.com>
2025-04-16 10:07:25 +0200

    commit

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://backend-mauve-pi-81.vercel.app'});
+const baseQuery = fetchBaseQuery({ 
+    baseUrl: 'https://backend-mauve-pi-81.vercel.app',
+    prepareHeaders: (headers) => {
+        const token = localStorage.getItem("jwt");
+    
+        if (token) {
+          headers.set("Authorization", Bearer ${token});
+        }
+    
+        return headers;
+      },
+      credentials: "include",
+});
 
 export const apiSlice = createApi({
     baseQuery,
